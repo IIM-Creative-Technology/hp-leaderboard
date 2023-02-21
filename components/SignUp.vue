3cfad2ba8298416169d3098d0183def1ad9db573
Nvcq <nicolas.mopin@gmail.com>
2023-02-21 16:53:47 +0100

    Sign up design

--- components/SignUp.vue
+++ components/SignUp.vue
@@ -1,19 +1,9 @@
 <template>
-    <div style="background-image:../static/assets/hp.jpg ;">
+    <!-- <div style="background-image:../static/assets/hp.jpg ;">
         <form @submit="register()" enctype="multipart/form-data" method="post">
-          <div class="names">
-            <div class="name">
-              <label for="firstname"><strong>First Name</strong></label>
-              <input type="text" name="firstname" v-model="firstname" />
-            </div>
-            <div class="name">
-              <label for="lastname"><strong>Last Name</strong></label>
-              <input type="text" name="lastname" v-model="lastname" />
-            </div>
-          </div>
           <div class="fields">
-            <label for="email"><strong>Email Address</strong></label>
-            <input type="email" name="email" v-model="email" />
+            <label for="nickname"><strong>Pseudo</strong></label>
+            <input type="text" name="nickname" v-model="nickname" />
 
             <label for="password"><strong>Password</strong></label>
             <input type="password" name="password" v-model="password" />
@@ -28,18 +18,270 @@
             </button>
           </div>
         </form>
+    </div> -->
+
+  <main class="main">
+    <div class="container">
+      <section class="wrapper">
+        <div class="heading">
+          <h1 class="text text-large">Sign Up</h1>
+        </div>
+        <form @submit="register()" enctype="multipart/form-data" method="post" name="signin" class="form">
+          <div class="input-control">
+            <label for="name" class="input-label" hidden>Nickname</label>
+            <input type="text" name="name" id="nickname" v-model="name" class="input-field" placeholder="Nickname">
+          </div>
+          <div class="input-control">
+            <label for="password" class="input-label" hidden>Password</label>
+            <input type="password" name="password" v-model="password" id="password" class="input-field" placeholder="Password">
+          </div>
+          <div class="input-control">
+            <button type="button" class="input-submit" style="margin: auto;" @click="register()">Sign Up</button>
+          </div>
+        </form>
+      </section>
     </div>
-    
+  </main>
+
 </template>
+
+<style>
+  :root {
+  --color-white: #ffffff;
+  --color-light: #f1f5f9;
+  --color-black: #121212;
+  --color-night: #001632;
+  --color-red: #f44336;
+  --color-blue: #1a73e8;
+  --color-gray: #80868b;
+  --color-grayish: #dadce0;
+  --shadow-normal: 0 1px 3px 0 rgba(0, 0, 0, 0.1),
+  	0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  --shadow-medium: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+  	0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  --shadow-large: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+  	0 4px 6px -2px rgba(0, 0, 0, 0.05);
+}
+
+html {
+  font-size: 100%;
+  font-size-adjust: 100%;
+  box-sizing: border-box;
+  scroll-behavior: smooth;
+}
+
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  box-sizing: inherit;
+  list-style: none;
+  list-style-type: none;
+  text-decoration: none;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-rendering: optimizeLegibility;
+}
+
+body {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 1rem;
+  font-weight: normal;
+  line-height: 1.5;
+  color: var(--color-black);
+  background: var(--color-light);
+}
+
+a,
+button {
+  font-family: inherit;
+  font-size: inherit;
+  line-height: inherit;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  background: none;
+  text-decoration: none;
+}
+
+img {
+  display: block;
+  width: 100%;
+  height: auto;
+  -o-object-fit: cover;
+     object-fit: cover;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 80rem;
+  min-height: 100vh;
+  width: 100%;
+  padding: 0 2rem;
+  margin: 0 auto;
+}
+
+.ion-logo-apple {
+  font-size: 1.65rem;
+  line-height: inherit;
+  margin-right: 0.5rem;
+  color: var(--color-black);
+}
+.ion-logo-google {
+  font-size: 1.65rem;
+  line-height: inherit;
+  margin-right: 0.5rem;
+  color: var(--color-red);
+}
+.ion-logo-facebook {
+  font-size: 1.65rem;
+  line-height: inherit;
+  margin-right: 0.5rem;
+  color: var(--color-blue);
+}
+
+.text {
+  font-family: inherit;
+  line-height: inherit;
+  text-transform: unset;
+  text-rendering: optimizeLegibility;
+}
+.text-large {
+  font-size: 2rem;
+  font-weight: 600;
+  color: var(--color-black);
+}
+.text-normal {
+  font-size: 1rem;
+  font-weight: 400;
+  color: var(--color-black);
+}
+.text-links {
+  font-size: 1rem;
+  font-weight: 400;
+  color: var(--color-blue);
+}
+.text-links:hover {
+  text-decoration: underline;
+}
+
+.main .wrapper {
+  max-width: 28rem;
+  width: 100%;
+  margin: 2rem auto;
+  padding: 2rem 2.5rem;
+  border: none;
+  outline: none;
+  border-radius: 0.25rem;
+  color: var(--color-black);
+  background: var(--color-white);
+  box-shadow: var(--shadow-large);
+}
+.main .wrapper .form {
+  width: 100%;
+  height: auto;
+  margin-top: 2rem;
+}
+.main .wrapper .form .input-control {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1.25rem;
+}
+.main .wrapper .form .input-field {
+  font-family: inherit;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: inherit;
+  width: 100%;
+  height: auto;
+  padding: 0.75rem 1.25rem;
+  border: none;
+  outline: none;
+  border-radius: 2rem;
+  color: var(--color-black);
+  background: var(--color-light);
+  text-transform: unset;
+  text-rendering: optimizeLegibility;
+}
+.main .wrapper .form .input-submit {
+  font-family: inherit;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: inherit;
+  cursor: pointer;
+  min-width: 40%;
+  height: auto;
+  padding: 0.65rem 1.25rem;
+  border: none;
+  outline: none;
+  border-radius: 2rem;
+  color: var(--color-white);
+  background: var(--color-blue);
+  box-shadow: var(--shadow-medium);
+  text-transform: capitalize;
+  text-rendering: optimizeLegibility;
+}
+.main .wrapper .striped {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem 0;
+}
+.main .wrapper .striped-line {
+  flex: auto;
+  flex-basis: auto;
+  border: none;
+  outline: none;
+  height: 2px;
+  background: var(--color-grayish);
+}
+.main .wrapper .striped-text {
+  font-family: inherit;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: inherit;
+  color: var(--color-black);
+  margin: 0 1rem;
+}
+.main .wrapper .method-control {
+  margin-bottom: 1rem;
+}
+.main .wrapper .method-action {
+  font-family: inherit;
+  font-size: 0.95rem;
+  font-weight: 500;
+  line-height: inherit;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: auto;
+  padding: 0.35rem 1.25rem;
+  outline: none;
+  border: 2px solid var(--color-grayish);
+  border-radius: 2rem;
+  color: var(--color-black);
+  background: var(--color-white);
+  text-transform: capitalize;
+  text-rendering: optimizeLegibility;
+  transition: all 0.35s ease;
+}
+.main .wrapper .method-action:hover {
+  background: var(--color-light);
+}
+</style>
 
 <script>
 import axios from 'axios'
 export default {
   data() {
     return {
-      firstname: '',
-      lastname: '',
-      email: '',
+      name: '',
       password: '',
       error: null,
     }
@@ -47,15 +289,12 @@ export default {
 
   methods: {
     async register() {
-        console.log("le boss");
       const data = {
-        firstname: this.firstname,
-        lastname: this.lastname,
-        email: this.email,
+        name: this.name,
         password: this.password,
       }
       axios
-        .post('localhost:3000/auth/register', data)
+        .post('https://hp-api-iim.azurewebsites.net/auth/register', data)
         .then((res) => {
           const userData = res.data
           this.$toasted.show('You have registered successfully', {
