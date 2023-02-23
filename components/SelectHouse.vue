131cafa1476e0caf85e113946dd660e3bfa9d9f4
Nvcq <nicolas.mopin@gmail.com>
2023-02-23 14:49:05 +0100

    fix houses

--- components/SelectHouse.vue
+++ components/SelectHouse.vue
@@ -2,9 +2,9 @@
     <main class="selectHouseMain">
         <div>
             <img src="../static/assets/snake.png" class="noOpacity" @click="$emit('select', 1)" :class="{opacity : imageHover !== 1 && imageHover !== null}" @mouseover="imageHover = 1" @mouseleave="imageHover = null" alt="Snake">
-            <img src="../static/assets/eagle.png" class="noOpacity" @click="$emit('select', 2)" alt="Eagle" :class="{opacity : imageHover !== 2 && imageHover !== null}" @mouseover="imageHover = 2" @mouseleave="imageHover = null">
-            <img src="../static/assets/badger.png" class="noOpacity" @click="$emit('select', 3)" alt="Badger" :class="{opacity : imageHover !== 3 && imageHover !== null}" @mouseover="imageHover = 3" @mouseleave="imageHover = null">
-            <img src="../static/assets/lion.png" class="noOpacity" @click="$emit('select', 4)" alt="Lion" :class="{opacity : imageHover !== 4 && imageHover !== null}" @mouseover="imageHover = 4" @mouseleave="imageHover = null">
+            <img src="../static/assets/eagle.png" class="noOpacity" @click="$emit('select', 3)" alt="Eagle" :class="{opacity : imageHover !== 3 && imageHover !== null}" @mouseover="imageHover = 3" @mouseleave="imageHover = null">
+            <img src="../static/assets/badger.png" class="noOpacity" @click="$emit('select', 4)" alt="Badger" :class="{opacity : imageHover !== 4 && imageHover !== null}" @mouseover="imageHover = 4" @mouseleave="imageHover = null">
+            <img src="../static/assets/lion.png" class="noOpacity" @click="$emit('select', 2)" alt="Lion" :class="{opacity : imageHover !== 2 && imageHover !== null}" @mouseover="imageHover = 2" @mouseleave="imageHover = null">
         </div>
         
     </main>
